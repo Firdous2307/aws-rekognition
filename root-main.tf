93c018d4eb5a20d74b7dab9a8c3244eeb7d3ab32
Firdous2307 <mohammedfirdousaraoye@gmail.com>
2024-01-19 09:35:47 +0000

    #11 test_bucket created successfully and added modules directory

--- root-main.tf
+++ root-main.tf
@@ -28,27 +28,12 @@ terraform {
 
 provider "aws" {
 }
-provider "random" {
-  # Configuration options
-}
 
-# [Random String Resource Docs]
-# Available at:(https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string)
-resource "random_string" "bucket_name" {
-  lower = true
-  upper = false
-  length   = 32
-  special  = false
-}
+resource "aws_s3_bucket" "firdous-test-bucket" {
+  bucket = "awesome-tf-test-bucket"
 
-# [AWS S3 Bucket Resource Docs]
-# Available at:(https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket)
-resource "aws_s3_bucket" "example" {
-# [S3 Bucket Naming Rules]
-# Available at:(https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console)
-  bucket = random_string.bucket_name.result
-}
-
-output "random_bucket_name" {
-  value = random_string.bucket_name.result
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
 }
