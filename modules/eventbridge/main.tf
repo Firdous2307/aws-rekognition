b168115eda57e3bc54251901a94402de4f254132
Firdous2307 <mohammedfirdousaraoye@gmail.com>
2024-03-28 15:05:10 +0000

    #18 eventbridge rule triggers lambda function on s3 object creation.

--- modules/eventbridge/main.tf
+++ modules/eventbridge/main.tf
@@ -10,21 +10,6 @@ variable "lambda_function_arn" {
   description = "ARN of the Lambda function"
 }
 
-# Import IAM role as data
-data "aws_iam_role" "iam_for_lambda" {
-  name = "iam_for_lambda"
-}
-
-# Reference existing Lambda function
-data "aws_lambda_function" "test_lambda" {
-  function_name = "lambda-demo-function"
-}
-
-# Reference existing S3 bucket
-data "aws_s3_bucket" "firdous_rekognition_image_bucket" {
-  bucket = var.bucket_name
-}
-
 
 resource "aws_cloudwatch_event_rule" "s3_event_rule" {
   name        = "s3_event_rule"
@@ -49,7 +34,7 @@ resource "aws_cloudwatch_event_target" "lambda_target" {
 resource "aws_lambda_permission" "allow_s3_invoke_lambda" {
   statement_id  = "AllowExecutionFromS3"
   action        = "lambda:InvokeFunction"
-  function_name = data.aws_lambda_function.test_lambda.arn 
+  function_name = var.lambda_function_arn
   principal     = "s3.amazonaws.com"
   source_arn    = aws_cloudwatch_event_rule.s3_event_rule.arn
 }
