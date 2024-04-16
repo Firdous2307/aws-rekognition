2258b55d8662ba465838329bb4419dbdb173069a
Firdous2307 <mohammedfirdousaraoye@gmail.com>
2024-04-16 16:21:28 +0000

    #21 ecr repo created

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -1,43 +1,8 @@
-/*
-variable "repository_name" {
-  description = "The name of the ECR repository"
-  type        = string
-}
-
 provider "aws" {
   region = "us-east-2"
 }
 
 resource "aws_ecr_repository" "private_repo" {
-  name = var.repository_name
-
+  name             = "rekognition-repo"
   image_tag_mutability = "MUTABLE"
-  scan_on_push         = false
-
-  lifecycle {
-    ignore_changes = [image_tag_mutability, scan_on_push]
-  }
-
-policy = <<EOF
-{
-  "Version": "2008-10-17",
-  "Statement": [
-    {
-      "Sid": "AllowPushPull",
-      "Effect": "Allow",
-      "Principal": "*",
-      "Action": [
-        "ecr:GetDownloadUrlForLayer",
-        "ecr:BatchGetImage",
-        "ecr:BatchCheckLayerAvailability",
-        "ecr:PutImage",
-        "ecr:InitiateLayerUpload",
-        "ecr:UploadLayerPart",
-        "ecr:CompleteLayerUpload"
-      ]
-    }
-  ]
 }
-EOF
-}
-*/
